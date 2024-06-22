5bffb23e6695dbf0141106526de756a9e3c4d8f1
Christinne <vanessa0christinne@gmail.com>
2024-06-23 00:27:37 +0200

    update: add pytests to class

--- tests/test_list.py
+++ tests/test_list.py
@@ -3,6 +3,11 @@ import sys
 
 from components.ui.cli_controller import main
 
-def test_list_files():
-    sys.argv = ["../entrypoint.py", "/users/christinne/developer"]
-    main()
+class TestList:
+    def test_list_files(self):
+        sys.argv = ["../entrypoint.py", "/users/christinne/developer"]
+        main()
+
+    def test_list_files_size(self):
+        sys.argv = ["../entrypoint.py", "/users/christinne/developer", "-s"]
+        main()
