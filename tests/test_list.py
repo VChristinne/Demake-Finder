6fc09010abd09d76c8d8d0d42cdf3f4093de54a8
Christinne <vanessa0christinne@gmail.com>
2024-06-23 12:29:07 +0200

    build: tests for invalid input

--- tests/test_list.py
+++ tests/test_list.py
@@ -8,6 +8,22 @@ class TestList:
         sys.argv = ["../entrypoint.py", "/users/christinne/developer"]
         main()
 
-    def test_list_files_size(self):
+    def test_list_size(self):
         sys.argv = ["../entrypoint.py", "/users/christinne/developer", "-s"]
         main()
+
+
+class TestUserInput:
+    @pytest.mark.xfail(reason="Invalid directory", raises=SystemExit)
+    def test_invalid_directory(self):
+        sys.argv = ["../entrypoint.py", "/invalid"]
+        main()
+
+    @pytest.mark.xfail(reason="Invalid argument", raises=SystemExit)
+    def test_invalid_args(self):
+        sys.argv = ["../entrypoint.py", "/users/christinne/developer", "-x"]
+        main()
+
+if __name__ == '__main__':
+    TestList()
+    TestUserInput()
