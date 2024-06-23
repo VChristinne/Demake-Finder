a7e10edc8b36229fb3c6ba9b7c2d0ff8b85815a3
Christinne <vanessa0christinne@gmail.com>
2024-06-23 17:15:36 +0200

    fix: error in if-elif for args

--- components/ui/cli_controller.py
+++ components/ui/cli_controller.py
@@ -1,12 +1,13 @@
 import argparse
 import sys
 
-from components.file_management.list.list_service import list_files, show_size
+from components.file_management.list.list_service import list_files, show_size, list_date
 from components.file_management.list.list_validator import is_valid_directory
 
 def main():
     parser = argparse.ArgumentParser(description="List files in a directory")
     parser.add_argument("directory", help="Directory to list")
+    parser.add_argument("-l", "--list", help="List files", action="store_true")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
     parser.add_argument("-s", "--size", help="Show file sizes", action="store_true")
     parser.add_argument("-d", "--date", help="Show file dates", action="store_true")
@@ -14,15 +15,12 @@ def main():
     directory = args.directory
 
     if is_valid_directory(directory):
-        if args.size:
-            files = show_size(directory)
-        else:
-            files = list_files(directory)
-        if isinstance(files, list):
-            for file in files:
-                print(f"├── {file}")
-        else:
-            print(files)
+        if args.list:
+            list_files(directory)
+        elif args.size:
+            show_size(directory)
+        elif args.date:
+            list_date(directory)
     else:
         print(f"Directory {directory} is not valid")
         sys.exit(1)
